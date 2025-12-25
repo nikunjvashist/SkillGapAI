8324a093f7fef43e06f77f8268191010260202f3
nikunjvashist <137261569+nikunjvashist@users.noreply.github.com>
2025-12-25 14:18:46 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,22 @@
+# Given Values
+a = 15
+b = 4
+c = 9
+d = 20
+nums = [4, 9, 15, 25]
+
+print("1.", a + b)                     # a + b
+print("2.", d - c)                     # d - c
+print("3.", b * 2)                     # b * 2
+print("4.", a % b)                     # a % b
+print("5.", c > b)                     # c > b
+print("6.", (a * b) - c)               # (a * b) - c
+print("7.", d // b)                    # d // b
+print("8.", (a > c) and (b < d))       # (a > c) and (b < d)
+print("9.", a in nums)                 # a in nums
+print("10.", c ** 2)                   # c ** 2
+print("11.", ((a + c) // b) + (d % a)) # ((a + c) // b) + (d % a)
+print("12.", (a ** b) // c)            # (a ** b) // c
+print("13.", (a < d) or (c == b))      # (a < d) or (c == b)
+print("14.", nums is nums)             # nums is nums
+print("15.", ((d // b) ** 2) - (a % c))# ((d // b) ** 2) - (a % c)
